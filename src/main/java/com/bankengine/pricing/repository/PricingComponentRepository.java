0dc3e8cf4924241bfb667133bf68225c6fbe8c12
sanjeev mishra <sanjusoftware@gmail.com>
2026-04-08 23:15:51 +0300

    refactor(pricing-component): add EntityGraph to optimize findByTypeIn query

--- src/main/java/com/bankengine/pricing/repository/PricingComponentRepository.java
+++ src/main/java/com/bankengine/pricing/repository/PricingComponentRepository.java
@@ -1,19 +1,19 @@
 package com.bankengine.pricing.repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.Query;
-
 import com.bankengine.common.repository.VersionableRepository;
 import com.bankengine.pricing.model.PricingComponent;
 import com.bankengine.pricing.model.PricingComponent.ComponentType;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PricingComponentRepository extends VersionableRepository<PricingComponent> {
     @EntityGraph(value = "component-with-tiers-values-conditions", type = EntityGraph.EntityGraphType.LOAD)
     @Query("SELECT DISTINCT pc FROM PricingComponent pc")
     List<PricingComponent> findAllWithDetailsBy();
 
+    @EntityGraph(value = "component-with-tiers-values-conditions", type = EntityGraph.EntityGraphType.LOAD)
     @Query("SELECT DISTINCT pc FROM PricingComponent pc WHERE pc.type IN :types")
     List<PricingComponent> findByTypeIn(List<ComponentType> types);
 }
